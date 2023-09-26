a34a2cfe55c490dab8d1ab50d91fb4a99c7a2b2b
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-09-26 23:32:28 +0530

    Create Count All Subarrays With Given Sum.cpp

---
+++ Count All Subarrays With Given Sum.cpp
@@ -0,0 +1,13 @@
+int findAllSubarraysWithGivenSum(vector < int > & nums, int k) {
+    // Write Your Code Here
+    unordered_map<int,int> mep;
+        mep[0]=1;
+        int count=0,presum=0;
+        for(int i=0;i<nums.size();i++){
+            presum+=nums[i];
+            int remove=presum-k;
+            count+=mep[remove];
+            mep[presum]++;
+        }
+        return count;
+    }
