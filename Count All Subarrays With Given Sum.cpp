be7a2b74def35159a7985118c0197f1440f498b5
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-11-18 21:57:33 +0530

    Update Count All Subarrays With Given Sum.cpp

--- Count All Subarrays With Given Sum.cpp
+++ Count All Subarrays With Given Sum.cpp
@@ -4,6 +4,7 @@ int findAllSubarraysWithGivenSum(vector < int > & nums, int k) {
         mep[0]=1;
         int count=0,presum=0;
         for(int i=0;i<nums.size();i++){
+            
             presum+=nums[i];
             int remove=presum-k;
             count+=mep[remove];
