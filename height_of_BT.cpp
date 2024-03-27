fa8abdbf0de4246fd49bf7c097968d7ac6c1231b
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-03-27 22:54:58 +0530

    Create height_of_BT.cpp

---
+++ height_of_BT.cpp
@@ -0,0 +1,31 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int heightOfBinaryTree(TreeNode<int> *root)
+{
+	// Write your code here.
+    if(!root) return 0;
+    int left=heightOfBinaryTree(root->left);
+    int right=heightOfBinaryTree(root->right);
+
+    return 1+max(left,right);
+}
