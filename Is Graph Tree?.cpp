ee010407aa50a407c8e10dbb77c931e8a319f39d
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-03-29 23:30:25 +0530

    Create  Is Graph Tree?.cpp

---
+++ Is Graph Tree?.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+bool isGraphTree(int n, vector<vector<int>> &edgeList)
+
+{
+
+// Write your code here
+
+int m=edgeList.size();
+
+if(m<n) return true;
+
+return false;
+
+}
