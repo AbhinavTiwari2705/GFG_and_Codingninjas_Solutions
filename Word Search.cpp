707a4aad70915979c245562d29c30b73369506f9
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-06-28 22:56:59 +0530

    Create Word Search.cpp

---
+++ Word Search.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    bool isWordExist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+private:
+    bool dfs(vector<vector<char>>& board, const string& word, int i, int j, int index) {
+        if (index == word.size()) {
+            return true;
+        }
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index]) {
+            return false;
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = '#';  // Mark the cell as visited
+
+        // Explore all four directions
+        bool found = dfs(board, word, i+1, j, index+1) ||
+                     dfs(board, word, i-1, j, index+1) ||
+                     dfs(board, word, i, j+1, index+1) ||
+                     dfs(board, word, i, j-1, index+1);
+
+        board[i][j] = temp;  // Unmark the cell
+
+        return found;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>board(n, vector<char>(m, '*'));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)
+				cin >> board[i][j];
+		string word;
+		cin >> word;
+		Solution obj;
+		bool ans = obj.isWordExist(board, word);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
