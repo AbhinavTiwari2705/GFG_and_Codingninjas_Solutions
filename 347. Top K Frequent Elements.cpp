c7db38e43651b7f164c23274346e1bdb69890961
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-08-04 22:18:05 +0530

    Create 347. Top K Frequent Elements.cpp

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool comp(int a,int b){
+        return a>b;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mep;
+        for(auto i:nums){
+            mep[i]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+        for(auto i:mep){
+            pq.push({i.second,i.first});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int> result;
+
+        while(!pq.empty()){
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        return result;
+
+        
+    }
+};
