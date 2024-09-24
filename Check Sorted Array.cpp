611b063a81752ec06c546696d2aabf8ec109a921
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-09-24 21:45:20 +0530

    Update and rename Check Sorted Array to Check Sorted Array.cpp

--- Check Sorted Array.cpp
+++ Check Sorted Array.cpp
@@ -2,7 +2,8 @@ int isSorted(int n, vector<int> a) {
     int i=0,j=1;
     while(j<n){
         if(a[i]<=a[j]){
-            i++,j++;
+            i++;
+            j++;
         }
         else if (a[i]>a[j]){
             return 0;
